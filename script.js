dfe569e1e61ac94f711299176527bb0a0a5b12fa
Gero <gersonhoa@gmail.com>
2022-04-08 11:34:42 -0300

    reupload : pixels art

--- script.js
+++ script.js
@@ -14,16 +14,6 @@ function generateColors() {
   }
   // Minha contribuição para a formula que vez por outra retornava numeros quebrados
   return randomColor;
-
-  /* randomColor receve "#" concatenado com o arredondamento para baixo (Math.floor())
-  de um numero aleatorio entre zero e um  (math.random())
-  Multiplicado pelo numero possivel de cores (*16777215)
-  Tudo isso convertido a uma string de base Hexadecimal (toString(16))
-   Referencias para generateColors
-  https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
-  https://css-tricks.com/snippets/javascript/random-hex-color/
-  https://stackoverflow.com/questions/1484506/random-color-generator
-  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/toString */
 }
 // gera uma cor Hex aleatoria
 function formBase(identf) {
